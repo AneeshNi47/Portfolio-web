19e178dd5701af5531aa0ced192c863531eeac4c
AneeshNi47 <dr2633@gmail.com>
2020-04-25 12:40:12 +0530

    added setting.py edit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -84,8 +85,10 @@ DATABASES = {
         'HOST': 'https://immense-falls-02774.herokuapp.com/db',
         'PORT': '5432',
     }
+    
 }
 
+django_heroku.settings(locals())
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
