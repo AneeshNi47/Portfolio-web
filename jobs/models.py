18d6c7c2aba3a06874e29eba3d98a26e6c7107a7
AneeshNi47 <dr2633@gmail.com>
2020-04-26 21:35:29 +0530

    added localhost to settings3

--- jobs/models.py
+++ jobs/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Job(models.Model):
-     title = models.CharField(max_length=200)
+     title = models.CharField(max_length=200, default='Title')
      image = models.ImageField(upload_to='image/')
-     summary = models.CharField(max_length=200)
+     summary = models.CharField(max_length=200, default='summary')
      job_url = models.URLField(max_length=200, default='SOME STRING')
