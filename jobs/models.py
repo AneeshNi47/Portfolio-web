685f9f19f52d4fdee6522fff6bc7f9666a5b8116
AneeshNi47 <dr2633@gmail.com>
2020-04-26 21:33:23 +0530

    added localhost to settings2

--- jobs/models.py
+++ jobs/models.py
@@ -6,4 +6,4 @@ class Job(models.Model):
      title = models.CharField(max_length=200)
      image = models.ImageField(upload_to='image/')
      summary = models.CharField(max_length=200)
-     job_url = models.URLField(max_length=200)
+     job_url = models.URLField(max_length=200, default='SOME STRING')
