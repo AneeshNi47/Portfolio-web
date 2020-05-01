68c84b503dd1aadd41715057849217bb08cbd343
AneeshNi47 <dr2633@gmail.com>
2020-05-01 12:51:38 +0530

    added new changes to blog table

--- blog/models.py
+++ blog/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Blog(models.Model):
-     title = models.CharField(max_length=200)
+     title = models.CharField(max_length=200, default='')
      pub_date = models.DateTimeField()
      body = models.TextField()
      image = models.ImageField(upload_to='image/')
+     hashtags1 = models.CharField(max_length=200, default='')
+     hashtags2 = models.CharField(max_length=200, default='')
+     hashtags3 = models.CharField(max_length=200, default='')
 
      def __str__(self):
           return self.title
