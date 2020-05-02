115ed01b49a3059e4eed9e7ba8881caebb5a8bf8
AneeshNi47 <dr2633@gmail.com>
2020-05-02 10:59:39 +0530

    corrected db related issue

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -7,9 +8,11 @@ class Blog(models.Model):
      pub_date = models.DateTimeField()
      body = models.TextField()
      image = models.ImageField(upload_to='image/')
+     #title = models.ForeignKey('Subject', default=1, on_delete=models.CASCADE)
      hashtags1 = models.CharField(max_length=200, default='')
      hashtags2 = models.CharField(max_length=200, default='')
      hashtags3 = models.CharField(max_length=200, default='')
+     #author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
 
      def __str__(self):
           return self.title
@@ -20,4 +23,9 @@ class Blog(models.Model):
      def pub_date_pretty(self):
           return self.pub_date.strftime('%b %e %Y')
 
+class Subject(models.Model):
+     stitle = models.CharField(max_length=200, default='null')
+     create_date = models.DateTimeField()
 
+     def __str__(self):
+          return self.stitle
