884bd4dceadf488d32c484c346636a57fef02eb3
AneeshNi47 <dr2633@gmail.com>
2020-05-02 21:33:30 +0530

    added weather API

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+import requests
 from .models import Blog
 from .models import testTable
 
@@ -7,8 +8,23 @@ def allblogs(request):
     blogs = Blog.objects
     items = testTable.objects
     last_blog = Blog.objects.all().last()
-    return render(request, 'blog/allblogs.html',{'blogs':blogs, 'last_blog':last_blog, 'items':items })
+    url = "https://community-open-weather-map.p.rapidapi.com/weather"
+    querystring = { "id":"2172797",
+                    "units":"metric",
+                    "q":"London,uk"
+                    }
+    headers = {
+        'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com",
+        'x-rapidapi-key': "1fe53a3500msh648ee33bcb45224p1791e1jsne51a930b6e9e"
+        }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    print(response.json())
+    weather = response.json()
+    return render(request, 'blog/allblogs.html',{'blogs':blogs, 'last_blog':last_blog, 'items':items, 'weather':weather})
 
 def detail(request, blog_id):
     detailblog = get_object_or_404(Blog, pk=blog_id)
     return render(request, 'blog/detail.html', {'blog' : detailblog})
+
+
+
