063d1a0073b08a3df62a5530e19025b3de77bee8
AneeshNi47 <dr2633@gmail.com>
2020-05-07 07:24:18 +0530

    added new Favicon files

--- blog/models.py
+++ blog/models.py
@@ -25,4 +25,4 @@ class Blog(models.Model):
           return self.body1[:200]
      
      def pub_date_pretty(self):
-          return self.pub_date.strftime('%b %e %Y')
+          return self.pub_date.strftime('%b %e, %Y')
--- blog/views.py
+++ blog/views.py
@@ -3,19 +3,8 @@ import requests
 from .models import Blog
 
 def allblogs(request):
-    x_forwarded_for = request.META.get('REMOTE_HOST')
-    print(x_forwarded_for)
     blogs = Blog.objects
     last_blog = Blog.objects.all().last()
-    url_ip = "https://ip-geo-location.p.rapidapi.com/ip/" + str(x_forwarded_for)
-    print(url_ip)
-    querystring_ip = {"format":"json"}
-    headers_ip = {
-        'x-rapidapi-host': "ip-geo-location.p.rapidapi.com",
-        'x-rapidapi-key': "567ab5fe7bmsh396072837da1a2cp161316jsnb5774dba1784"
-        }
-    response_ip = requests.request("GET", url_ip, headers=headers_ip, params=querystring_ip)
-    response_ip = response_ip.json()
     url = "https://api.openweathermap.org/data/2.5/weather?"
     querystring = { "q":"Kerala",
                     "appid":"f2c22ffb64b399b3a2aecccfe3fd34f4"
@@ -27,12 +16,24 @@ def allblogs(request):
                         'blogs':blogs, 
                         'last_blog':last_blog, 
                         'weather':weather,
-                        'weather_desc':weather["weather"][0]["description"], 
-                        'addres':response_ip, 
-                        'ip_addres':x_forwarded_for
+                        'weather_desc':weather["weather"][0]["description"]
                         }
                     )
 
 def detail(request, blog_id):
     detailblog = get_object_or_404(Blog, pk=blog_id)
     return render(request, 'blog/detail.html', {'blog' : detailblog})
+
+
+
+    #x_forwarded_for = request.META.get('REMOTE_HOST')
+    #print(x_forwarded_for)
+    #url_ip = "https://ip-geo-location.p.rapidapi.com/ip/" + str(x_forwarded_for)
+    #print(url_ip)
+    #querystring_ip = {"format":"json"}
+    #headers_ip = {
+        #'x-rapidapi-host': "ip-geo-location.p.rapidapi.com",
+        #'x-rapidapi-key': "567ab5fe7bmsh396072837da1a2cp161316jsnb5774dba1784"
+        #}
+    #response_ip = requests.request("GET", url_ip, headers=headers_ip, params=querystring_ip)
+    #response_ip = response_ip.json()
