f6034d625cf69e131b549fd596eb1314160b6db8
AneeshNi47 <dr2633@gmail.com>
2020-05-12 22:33:32 +0530

    changes in whatsap link:

--- jobs/admin.py
+++ jobs/admin.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 
 from .models import Job
 from .models import Vistor
+from .models import JobImage
 
 admin.site.register(Job)
 admin.site.register(Vistor)
+admin.site.register(JobImage)
--- jobs/models.py
+++ jobs/models.py
@@ -8,6 +8,13 @@ class Job(models.Model):
      summary = models.CharField(max_length=200, default='summary')
      job_url = models.URLField(max_length=200, default='SOME STRING')
 
+class JobImage(models.Model):
+     blog_id = models.ForeignKey(Job, on_delete=models.CASCADE, default=1)
+     img_label= models.CharField(max_length=200, default='Title')
+     image = models.ImageField(upload_to='image/')
+     alt_description = models.CharField(max_length=200, default='')
+     img_description = models.CharField(max_length=200, default='')
+
 
 class Vistor(models.Model):
      userip = models.CharField(max_length=200)
